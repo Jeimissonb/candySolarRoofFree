6120ca9eb7f45a991bfe19b4d12e56b4ec2c1197
Jeimissonb <jeimissonb@hotmail.com>
2022-02-03 02:55:22 -0300

    Page root

---
+++ frontend/src/components/Model/useModel.ts
@@ -0,0 +1,16 @@
+import { useCallback, useContext, useEffect } from "react";
+import ModelsContext from "./ModelsContext";
+
+export default function useModel(modelName: string) {
+  const {registerModel, unregisterModel, getModelByName} = useContext(ModelsContext);
+
+  useEffect (()=>{
+    return unregisterModel(modelName);
+  },[modelName, unregisterModel])
+
+  const getModel = useCallback(()=>{
+    return getModelByName(modelName)
+  },[getModelByName, modelName])
+
+  return {registerModel, getModel}
+}
